07c801a1cd80181cc136c133624a4f95ecce2616
Sovik211 <vital.gurov2017@gmail.com>
2026-01-28 19:26:25 +0800

    Создание ItemCardComponent

---
+++ app/src/main/java/ru/habit/tracker/presentation/ui/element/ItemCard.kt
@@ -0,0 +1,11 @@
+package ru.habit.tracker.presentation.ui.element
+
+import androidx.compose.material3.Card
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ItemCard(){
+    Card(){
+
+    }
+}
