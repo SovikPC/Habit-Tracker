526b3cd50b40bebeb15517e821409390d3d20ceb
Sovik211 <vital.gurov2017@gmail.com>
2026-01-28 17:25:56 +0800

    Добавление Room базы данных

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,8 @@ dependencies {
     implementation("androidx.room:room-runtime:2.8.4")
     ksp("androidx.room:room-compiler:2.8.4")
     implementation("androidx.room:room-ktx:2.8.4")
+    testImplementation("androidx.room:room-testing:2.8.4")
+    implementation("androidx.room:room-paging:2.8.4")
 
 
     implementation(libs.androidx.core.ktx)
