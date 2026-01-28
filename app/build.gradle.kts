b181381669fba075511a7315f7bd8843aaa8933a
Sovik211 <vital.gurov2017@gmail.com>
2026-01-28 19:16:10 +0800

    Настройка gradle (Добавление ksp, Dagger Hilt, Room, Jetpack Compose, Paging 3)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,12 +1,18 @@
 plugins {
     alias(libs.plugins.android.application)
     id("com.google.devtools.ksp")
+    id("com.google.dagger.hilt.android")
+    alias(libs.plugins.compose.compiler)
 }
 
 android {
     namespace = "ru.habit.tracker"
     compileSdk {
         version = release(36)
+    }
+
+    buildFeatures {
+        compose = true
     }
 
     defaultConfig {
@@ -35,6 +41,39 @@ android {
 }
 
 dependencies {
+
+    //Paging 3
+    val paging_version = "3.3.6"
+    implementation("androidx.paging:paging-runtime:$paging_version")
+    implementation("androidx.paging:paging-compose:3.4.0-rc01")
+    testImplementation("androidx.paging:paging-common:${paging_version}")
+
+    //Jetpack Compose
+    val composeBom = platform("androidx.compose:compose-bom:2025.12.00")
+    implementation(composeBom)
+    implementation("androidx.compose.material:material-icons-core")
+    implementation("androidx.compose.material:material-icons-extended")
+    androidTestImplementation(composeBom)
+    //Jetpack Compose android studio preview support
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    // UI Tests
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
+    // Optional - Integration with activities
+    implementation("androidx.activity:activity-compose:1.11.0")
+    // Optional - Integration with ViewModels
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.5")
+
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.material3.adaptive.navigation.suite)
+
+    //Dagger Hilt
+    implementation("com.google.dagger:hilt-android:2.59")
+    ksp("com.google.dagger:hilt-android-compiler:2.59")
 
     //Room
     implementation("androidx.room:room-runtime:2.8.4")
