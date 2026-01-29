c59a1ae4d40498a16c1e435d0d13173657fd8687
Sovik211 <vital.gurov2017@gmail.com>
2026-01-29 17:55:34 +0800

    Создание ItemCard

--- app/src/main/java/ru/habit/tracker/presentation/ui/element/ItemCard.kt
+++ app/src/main/java/ru/habit/tracker/presentation/ui/element/ItemCard.kt
@@ -1,11 +1,80 @@
 package ru.habit.tracker.presentation.ui.element
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Composable
-fun ItemCard(){
-    Card(){
-
+fun ItemCard(
+    content: @Composable () -> Unit = {}
+){
+    Card(
+        modifier = Modifier.padding(15.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.surface.copy(alpha = 0.5f)
+        ),
+        shape = RoundedCornerShape(12.dp)
+    ){
+        content()
     }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun PreviewItemCard(){
+    ItemCard(
+        content = {
+            Row(
+                modifier = Modifier.fillMaxWidth()
+                    .padding(5.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Box(){
+                    Checkbox(
+                        checked = false,
+                        onCheckedChange = {}
+                    )
+                }
+                Column(
+                    modifier = Modifier.clickable(
+                        onClick = {}
+                    )
+                        .weight(1f)
+                ){
+                    Text(
+                        text = "Title",
+                        style = MaterialTheme.typography.bodyLarge,
+                        fontWeight = FontWeight.Medium,
+                        color = MaterialTheme.colorScheme.onSurface
+                    )
+                    Spacer(modifier = Modifier.height(4.dp))
+                    Text(
+                        text = "Description",
+                        style = MaterialTheme.typography.bodySmall,
+                        color = MaterialTheme.colorScheme.tertiary
+                    )
+                }
+            }
+        }
+    )
 }
