8c3e8ffbba5af042d388a68435bd268515e38715
Sovik211 <vital.gurov2017@gmail.com>
2026-01-29 17:58:09 +0800

    Добавление Firebase в проект

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,5 @@ plugins {
     id("com.google.devtools.ksp") version "2.3.4" apply false
     id("com.google.dagger.hilt.android") version "2.59" apply false
     alias(libs.plugins.compose.compiler) apply false
+    id("com.google.gms.google-services") version "4.4.4" apply false
 }
