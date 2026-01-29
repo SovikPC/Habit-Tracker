9786b0b2b4ef11b00ac8c1ded4441b6e25a25a60
Sovik211 <vital.gurov2017@gmail.com>
2026-01-29 17:54:41 +0800

    Создание файла со шрифтами

---
+++ app/src/main/java/ru/habit/tracker/presentation/ui/theme/Fonts.kt
@@ -0,0 +1,45 @@
+package ru.habit.tracker.presentation.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import ru.habit.tracker.R
+
+val BebasNeue = FontFamily(
+    Font(R.font.bebas_neue_regular, FontWeight.Normal)
+)
+
+val Orbitron = FontFamily(
+    Font(R.font.orbitron_variable_font_wght, FontWeight.Normal)
+)
+
+val Audiowide = FontFamily(
+    Font(R.font.audiowide_regular, FontWeight.Normal)
+)
+
+val Rajdhani = FontFamily(
+    Font(R.font.rajdhani_bold, FontWeight.Bold),
+    Font(R.font.rajdhani_light, FontWeight.Light),
+    Font(R.font.rajdhani_medium, FontWeight.Medium),
+    Font(R.font.rajdhani_regular, FontWeight.Normal),
+    Font(R.font.rajdhani_semi_bold, FontWeight.SemiBold)
+)
+
+val Exo2 = FontFamily(
+    Font(R.font.exo2_variable_font_wght, FontWeight.Normal),
+    Font(R.font.exo2_italic_variable_font_wght, FontWeight.Thin),
+)
+
+val TitilliumWeb = FontFamily(
+    Font(R.font.titillium_web_black, FontWeight.Normal),
+    Font(R.font.titillium_web_bold, FontWeight.Bold),
+    Font(R.font.titillium_web_extra_light, FontWeight.ExtraLight),
+    Font(R.font.titillium_web_italic, FontWeight.Thin),
+    Font(R.font.titillium_web_light, FontWeight.Light),
+    Font(R.font.titillium_web_regular, FontWeight.Normal),
+    Font(R.font.titillium_web_semi_bold, FontWeight.SemiBold)
+)
+
+val RussoOne = FontFamily(
+    Font(R.font.russo_one_regular, FontWeight.Normal)
+)
