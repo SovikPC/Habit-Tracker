1475c029487ed59252166924c717cb33856e7b31
Sovik211 <vital.gurov2017@gmail.com>
2026-01-29 12:02:21 +0800

    Добавление цветовой палитры интерфейса

--- app/src/main/java/ru/habit/tracker/presentation/ui/theme/Color.kt
+++ app/src/main/java/ru/habit/tracker/presentation/ui/theme/Color.kt
@@ -9,3 +9,12 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val SlDarkBlue = Color(0xFF0A0E17)    // Основной темный фон
+val SlNavy = Color(0xFF121B2E)        // Вторичный темный
+val SlElectricBlue = Color(0xFF00D4FF) // Акцентный синий (система)
+val SlPurple = Color(0xFF8A2BE2)      // Пурпурный (магия/навыки)
+val SlRed = Color(0xFFFF4757)         // Красный (опасность/HP)
+val SlGold = Color(0xFFFFD700)        // Золотой (уровни/достижения)
+val SlGray = Color(0xFF2D3748)        // Серый для карточек
+val SlLightGray = Color(0xFF4A5568)   // Светло-серый текст
